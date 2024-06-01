0437eeed559fa68cbbf4659539d84412140dbe3b
Travis Ormsby <t.ormsby@esri.com>
2024-05-31 19:23:35 -0500

    use template file to create kubeconfig

--- create-kubeconfigs.sh
+++ create-kubeconfigs.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
+cluster=$(kubectl config view --minify -o jsonpath="{.contexts[0].context.cluster}")
+server=$(kubectl config view --minify -o jsonpath="{.clusters[0].cluster.server}")
+
+# create certificate authority cert
+kubectl config view --minify --flatten -o jsonpath="{.clusters[0].cluster.certificate-authority-data}" | base64 --decode > ca_cert.pem
+
+
 for i in $(seq -f '%02.f' $1 $2); do
   name=arcgis${i}
   csrname=${name}-$(date +%s)
@@ -23,8 +30,9 @@ EOF
   kubectl certificate approve $csrname
   kubectl get csr $csrname -o jsonpath='{.status.certificate}'| base64 -d > ${name}.crt
   cp configtemplate ${name}-config
+  kubectl config --kubeconfig ${name}-config set-cluster $cluster --server=$server --certificate-authority=ca_cert.pem --embed-certs=true 
   kubectl config --kubeconfig ${name}-config set-credentials $name --client-key ${name}.key --client-certificate=${name}.crt --embed-certs=true
-  kubectl config --kubeconfig ${name}-config set-context $name --cluster=kube --user=$name
+  kubectl config --kubeconfig ${name}-config set-context $name --cluster=$cluster --user=$name
   kubectl config --kubeconfig ${name}-config use-context $name
   rm ${name}.*
 done
